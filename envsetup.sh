e477baa4af830c0cdcb16118d2c4af98d0271723
Mohammad Anwari <mdamt@mdamt.net>
2015-02-05 19:06:14 +0700

    Get proper RIDON_BUILD var

--- envsetup.sh
+++ envsetup.sh
@@ -76,8 +76,8 @@ function check_product()
         return
     fi
 
-    if (echo -n $1 | grep -q -e "^cm_") ; then
-       RIDON_BUILD=$(echo -n $1 | sed -e 's/^cm_//g')
+    if (echo -n $1 | grep -q -e "^ridon_") ; then
+       RIDON_BUILD=$(echo -n $1 | sed -e 's/^ridon_//g')
        export BUILD_NUMBER=$((date +%s%N ; echo $RIDON_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
     else
        RIDON_BUILD=
